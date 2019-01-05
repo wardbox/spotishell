0f79246b18e9a44724f0d52a6837c3f4da4adce1
wardbox <empirebillboard@gmail.com>
2019-01-04 18:21:55 -0700

    get-userprofile
    already had get-myprofile done

---
+++ spotishell/Public/Get-UserProfile.ps1
@@ -0,0 +1,29 @@
+function Get-UserProfile {
+  <#
+  .SYNOPSIS
+    Get public profile information about a Spotify user.
+  .EXAMPLE
+    PS C:\> Get-UserProfile -Username "myusername"
+   Gets the public user profile information about myusername
+  .PARAMETER Username
+  This should be a string.
+  Username of the spotify user we want to search for
+  #>
+  param (
+    # Username
+    [Parameter(Mandatory)]
+    [String]
+    $Username
+  )
+  
+  Write-Verbose "Attempting to return profile of user with ID $Username"
+  $Method = "Get"
+  $Uri = "https://api.spotify.com/v1/users/" + $Username
+
+  try {
+    $Response = Send-SpotifyCall -Method $Method -Uri $Uri -ErrorAction Stop
+    return $Response
+  } catch {
+    Write-Warning "Failed sending Spotify API call for function Get-UserProfile"
+  }
+}
