f657f258906c5dbd3130d1737d24cd8cafd08f98
wardbox <dylan.kappler@gmail.com>
2019-01-09 19:15:04 -0800

    set-repeatmode but IT ISNT DONE

---
+++ spotishell/Public/Set-RepeatMode.ps1
@@ -0,0 +1,44 @@
+function Suspend-Playback {
+  <#
+  .SYNOPSIS
+    Pause playback on the user’s account.
+  .EXAMPLE
+    PS C:\> Suspend-Playback -Username "blahblah"
+    Pauses playback for user with username "blahblah"
+  .PARAMETER Username
+    Username of the spotify user we want to pause playback for
+  .PARAMETER DeviceId
+    The id of the device this command is targeting.
+  .PARAMETER State
+
+  #>
+  param (
+    # Username
+    [Parameter(Mandatory)]
+    [string]
+    $Username,
+
+    # Device ID that we want to pause playback on
+    [Parameter(Mandatory = $false)]
+    [array]
+    $DeviceId,
+
+    # State
+  )
+
+  Write-Verbose "Attempting to pause playback for user with username $Username"
+  $Method = "Put"
+  $Uri = "https://api.spotify.com/v1/me/player/pause"
+  if ($DeviceId) {
+    $Uri = "?device_id=$DeviceId"
+  }
+  $AccessToken = Get-SpotifyUserAccessToken -Username $Username
+  $Auth = @{
+    Authorization = "Bearer $($AccessToken.access_token)"
+  }
+
+  $Response = Send-SpotifyCall -Method $Method -Uri $Uri -Header $Auth -ErrorAction Stop
+  if ($Response.StatusCode -ne "204") {
+    return $Response
+  }
+}
