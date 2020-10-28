d8c37ef7f8a7d61ae7cde7813820c08cedf6da03
DomoChip <domochip@hotmail.com>
2020-10-28 11:51:29 +0100

    Update Invoke-SpotishellLogo

--- spotishell/Public/Invoke-SpotishellLogo.ps1
+++ spotishell/Public/Invoke-SpotishellLogo.ps1
@@ -1,22 +1,21 @@
-function Invoke-SpotishellLogo { 
-  <#
+<#
     .SYNOPSIS
-      Display a ascii spotishell logo on screen.
+        Display a ascii spotishell logo on screen.
     .EXAMPLE
-      PS C:\> Invoke-Ascii
-      Display a ascii spotishell logo on screen.
-    #>
-    
-  
-  Write-Host -ForegroundColor Green "    
-     _______  _______  _______  _______  ___   _______  __   __  _______  ___      ___     
-    |       ||       ||       ||       ||   | |       ||  | |  ||       ||   |    |   |    
-    |  _____||    _  ||   _   ||_     _||   | |  _____||  |_|  ||    ___||   |    |   | 
-    | |_____ |   |_| ||  | |  |  |   |  |   | | |_____ |       ||   |___ |   |    |   |  
-    |_____  ||    ___||  |_|  |  |   |  |   | |_____  ||       ||    ___||   |___ |   |___
-     _____| ||   |    |       |  |   |  |   |  _____| ||   _   ||   |___ |       ||       |
-    |_______||___|    |_______|  |___|  |___| |_______||__| |__||_______||_______||_______|        
-    https://github.com/wardbox/spotishell
-    "
+        PS C:\> Invoke-Ascii
+        Display a ascii spotishell logo on screen.
+#>
+function Invoke-SpotishellLogo { 
+
+    Write-Host -ForegroundColor Green "    
+         _______  _______  _______  _______  ___   _______  __   __  _______  ___      ___     
+        |       ||       ||       ||       ||   | |       ||  | |  ||       ||   |    |   |    
+        |  _____||    _  ||   _   ||_     _||   | |  _____||  |_|  ||    ___||   |    |   | 
+        | |_____ |   |_| ||  | |  |  |   |  |   | | |_____ |       ||   |___ |   |    |   |  
+        |_____  ||    ___||  |_|  |  |   |  |   | |_____  ||       ||    ___||   |___ |   |___
+         _____| ||   |    |       |  |   |  |   |  _____| ||   _   ||   |___ |       ||       |
+        |_______||___|    |_______|  |___|  |___| |_______||__| |__||_______||_______||_______|        
+        https://github.com/wardbox/spotishell
+        "
    
-  }
+}
