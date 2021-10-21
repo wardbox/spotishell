81d6685912f33c7a2a73a2055ff24175d87bdc69
DomoChip <domochip@hotmail.com>
2021-10-21 05:01:43 +0200

    Add Initialize-SpotifyApplication

---
+++ Spotishell/Public/Initialize-SpotifyApplication.ps1
@@ -0,0 +1,22 @@
+<#
+    .SYNOPSIS
+        Initializes Spotify Application
+    .DESCRIPTION
+        Initializes Spotify Application by retrieving and storing Spotify Access Token for an Application if it is missing or invalid in the local store
+        The retrieval process follows the Authorization Code Flow (https://developer.spotify.com/documentation/general/guides/authorization-guide/#authorization-code-flow)
+    .EXAMPLE
+        PS C:\> Initialize-SpotifyApplication -ApplicationName 'dev'
+        Looks for the Access Token of the Application named 'dev' and runs the Authorization Code Flow if it's missing or invalid
+    .PARAMETER ApplicationName
+        Specifies the Spotify Application Name (otherwise default is used)
+#>
+function Initialize-SpotifyApplication {
+
+    [CmdletBinding()]
+    param (
+        [String]
+        $ApplicationName
+    )
+
+    Get-SpotifyAccessToken -ApplicationName $ApplicationName
+}
